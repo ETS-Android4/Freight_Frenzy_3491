96ffef56c473b0908cff2e6bc269c999ceaa6fc8
BigShonner9620 <49296775+BigShonner9620@users.noreply.github.com>
2020-12-21 17:05:03 -0800

    adfdasgghdfgasdfgh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamecode/BeyonceTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamecode/BeyonceTeleOp.java
@@ -19,6 +19,9 @@ public class BeyonceTeleOp extends LinearOpMode {
     //probably going to be useful. if it aint broke, dont fix it
     private ElapsedTime runtime = new ElapsedTime();
 
+
+
+
     //motors
     private DcMotor frontLeft = null;
     private DcMotor frontRight = null;

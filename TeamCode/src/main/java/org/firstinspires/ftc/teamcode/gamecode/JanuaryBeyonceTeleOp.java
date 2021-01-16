a469073edf46f6aa5696ad080066b5e526875583
BigShonner9620 <49296775+BigShonner9620@users.noreply.github.com>
2021-01-16 12:54:53 -0800

    Organized repo, made change to slow button calculations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamecode/JanuaryBeyonceTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamecode/JanuaryBeyonceTeleOp.java
@@ -1,113 +1,109 @@
 package org.firstinspires.ftc.teamcode.gamecode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Robots.Beyonce;
+import org.firstinspires.ftc.teamcode.opmodesupport.TeleOpMode;
 
 
 @TeleOp
-public class JanuaryBeyonceTeleOp extends LinearOpMode {
+public class JanuaryBeyonceTeleOp extends TeleOpMode {
+    Beyonce beyonce;
 
-    //Import Beyonce class
-    Beyonce beyonce = new Beyonce();
+    public void initialize() {
+        //Create object of beyonce
+        beyonce = new Beyonce();
+        beyonce.FrontLeft.setReverse(true);
+        beyonce.BackLeft.setReverse(true);
 
-    private ElapsedTime runtime = new ElapsedTime();
-
-    @Override
-    public void runOpMode() {
         //Indicate that the program is running
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+    }
 
-        //Gives user 2 drive control options
-        boolean leftHanded = true;
+    //Gives user 2 drive control options
+    boolean leftHanded = true;
 
-        //Standard code
-        waitForStart();
-        runtime.reset();
+    //Declaring drive variables
+    double horizontal;
+    double vertical;
+    double pivot;
 
-        double pivot;
-        double horizontal;
-        double vertical;
+    public void loopOpMode() {
+        //Drive control option one
+        if (leftHanded) {
+            pivot = gamepad1.left_stick_x;
+            horizontal = -gamepad1.right_stick_x;
+            vertical = gamepad1.right_stick_y;
 
-        //While opMode is active do the stuff in the while loop
-        if(opModeIsActive()){
-        while (opModeIsActive()) {
-
-            //Drive control option one
-            if (leftHanded) {
-                if (gamepad1.left_bumper) {
-                    pivot = gamepad1.left_stick_x / 3;
-                    horizontal = -gamepad1.right_stick_x / 3;
-                    vertical = gamepad1.right_stick_y / 3;
-                } else {
-                    pivot = gamepad1.left_stick_x;
-                    horizontal = -gamepad1.right_stick_x;
-                    vertical = gamepad1.right_stick_y;
-                }
-                if (gamepad1.dpad_right) {
-                    leftHanded = false;
-                }
-
-                //Drive control option two
-            } else {
-                if (gamepad1.right_trigger > 0) {
-                    pivot = gamepad1.right_stick_x / 3;
-                    horizontal = -gamepad1.left_stick_x / 3;
-                    vertical = gamepad1.left_stick_y / 3;
-                } else {
-                    pivot = gamepad1.right_stick_x;
-                    horizontal = -gamepad1.left_stick_x;
-                    vertical = gamepad1.left_stick_y;
-                }
-/*nice*/        if (gamepad1.dpad_left) {
-                    leftHanded = true;
-                }
+            if (gamepad1.dpad_right) {
+                leftHanded = false;
             }
 
-            //Drive calculations
+        //Drive control option two
+        } else {
+            pivot = gamepad1.right_stick_x;
+            horizontal = -gamepad1.left_stick_x;
+            vertical = gamepad1.left_stick_y;
+
+            if (gamepad1.dpad_left) {
+                leftHanded = true;
+            }
+        }
+
+        //Drive calculations
+
+        if (gamepad1.right_trigger > 0) {
+            beyonce.FrontLeft.setPower((pivot + (vertical + horizontal))/5);
+            beyonce.FrontRight.setPower((-pivot + (vertical - horizontal))/5);
+            beyonce.BackLeft.setPower((pivot + (vertical - horizontal))/5);
+            beyonce.BackRight.setPower((-pivot + (vertical + horizontal))/5);
+            telemetry.addData("s", "slow");
+        } else {
             beyonce.FrontLeft.setPower(pivot + (vertical + horizontal));
             beyonce.FrontRight.setPower(-pivot + (vertical - horizontal));
             beyonce.BackLeft.setPower(pivot + (vertical - horizontal));
             beyonce.BackRight.setPower(-pivot + (vertical + horizontal));
+            telemetry.addData("s", "fast");
+/*nice*/}
 
-            //Wobble grabber Arm
-            beyonce.Arm.setPower(gamepad2.right_stick_y / 4);
 
-            //Wobble grabber Claw
-            if (gamepad2.x) {
-                beyonce.ClawOpen();
-            } else if (gamepad2.b){
-                beyonce.ClawClose();
-            }
+        //Wobble grabber Arm
+        beyonce.Arm.setPower(gamepad2.right_stick_y / 4);
 
-            //Ring pusher
-            if (gamepad2.right_trigger > 0) {
-                beyonce.RingPusherExtend();
-            } else {
-                beyonce.RingPusherRetract();
-            }
+        //Wobble grabber Claw
+        if (gamepad2.x) {
+            beyonce.ClawClose();
+        } else if (gamepad2.b){
+            beyonce.ClawOpen();
+        }
 
-            //Shooter ramp
-            beyonce.moveRamp(gamepad1.left_stick_y);
+        if (gamepad2.y) {
+            beyonce.Led.setPower(0);
+        }
+        beyonce.Led.setPower(1);
 
-            //Shooter
-            if (gamepad2.dpad_right) {
-                beyonce.ShooterOn();
-            }
-            if (gamepad2.dpad_left) {
-                beyonce.ShooterOff();
-            }
+        //Ring pusher
+        if (gamepad2.right_trigger > 0) {
+            beyonce.RingPusherExtend();
+        } else {
+            beyonce.RingPusherRetract();
+        }
 
-            //Keeps user updated
-            telemetry.addData("Motors", "horizontal (%.2f), vertical (%.2f), pivot (%.2f)\nslow button: (%.2f)", horizontal, vertical, pivot, gamepad1.right_trigger);
-            telemetry.update();
-        }}
+        //Shooter ramp
+        beyonce.moveRamp(gamepad1.left_stick_y);
+
+        //Shooter
+        if (gamepad2.dpad_right) {
+            beyonce.ShooterOn();
+        }
+        if (gamepad2.dpad_left) {
+            beyonce.ShooterOff();
+        }
+
+        //Keeps user updated
+        telemetry.addData("Motors", "horizontal (%.2f), vertical (%.2f), pivot (%.2f)\nslow button: (%.2f)", horizontal, vertical, pivot, gamepad1.right_trigger);
+        telemetry.update();
+
     }
 }

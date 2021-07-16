9ccd47ebe031ae734638f1cb6f94536fc1dca7b3
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2021-07-16 16:40:08 -0700

    Cleaned up "TestColourSense.java" program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/zSetUp/TestColourSense.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/zSetUp/TestColourSense.java
@@ -13,12 +13,10 @@ import org.firstinspires.ftc.teamcode.opMode_Support.AutoOpMode;
 public class TestColourSense extends AutoOpMode {
     org.firstinspires.ftc.teamcode.Robots.Beyonce Beyonce;
 
-
     private VoltageSensor ExpansionHub2_VoltageSensor;
 
-
     @Override
-    public void runOp(){
+    public void runOp() {
         ExpansionHub2_VoltageSensor = hardwareMap.get(VoltageSensor.class, "Expansion Hub 2");
 
         Beyonce beyonce = new Beyonce();
@@ -32,80 +30,10 @@ public class TestColourSense extends AutoOpMode {
 
         colorSensorL.enableLed(true);
 
-//        beyonce.Beat(-0.2);
-//        sleep(100);
-//        beyonce.Stop();
-
-
         while (opModeIsActive() ){
             telemetry.addData("red", colorSensorL.red());
         }
         beyonce.Stop();
-
-
-
-//        //telemetry.addData("blue", colorSensorL.blue());
-//      //  telemetry.addData("green", colorSensorL.green());
-//        //telemetry.addData("light", colorSensorL.alpha());
-//       // telemetry.addData("4 colour channels", colorSensorL.argb());
-//            }
-
-//        beyonce.StrafeLeft(0.2);
-//        telemetry.addData("staus", "left");
-//        sleep(1000);
-//        beyonce.Stop();
-//
-//        sleep(200);
-//
-//        beyonce.DriveForward(0.1);
-//        telemetry.addData("staus", "forward");
-//        sleep(700);
-//        beyonce.Stop();
-//
-//        sleep(300);
-//
-//        clearTimer(1);
-//
-//        while (opModeIsActive() && 38 > colorSensorL.red() && getSeconds(1) < 4){
-//            telemetry.addData("status", getSeconds(1));
-//            telemetry.addData("red", colorSensorL.red());
-//
-//            beyonce.StrafeRight(0.1);
-//        }
-//        telemetry.addData("staus", "white");
-//        telemetry.addData("red", colorSensorL.red());
-//
-//        beyonce.Stop();
-
-//nice    sleep(500);
-//
-//        beyonce.StrafeLeft(0.1);
-//        sleep(1250);
-//        beyonce.Stop();
-//
-//        sleep(200);
-//
-//        beyonce.DriveBackward(0.1);
-//        sleep(1300);
-//        beyonce.Stop();
-//
-//        sleep(300);
-//
-//        beyonce.GrabberDown();
-//
-//        sleep(600);
-//
-//        beyonce.DriveForward(0.3);
-//        sleep(300);
-//        beyonce.Stop();
-
-
-
-
-
-
-        //red in between like 30 and 36
-
     }
 
     double getBatteryVoltage() {
@@ -116,7 +44,6 @@ public class TestColourSense extends AutoOpMode {
                 result = Math.min(result, voltage);
             }
         }
-        return result;}
-
-
+        return result;
+    }
 }

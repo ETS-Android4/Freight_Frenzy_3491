840413dfd94234b9a1a43db0bfd4e9c89d2bb008
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2021-07-22 18:02:44 -0700

    Changed program to show camera view in "EasyOpenCVExample_ControlHub.java".

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/zReference/EasyOpenCVExample_ControlHubWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/zReference/EasyOpenCVExample_ControlHubWebcam.java
@@ -61,6 +61,57 @@ public class EasyOpenCVExample_ControlHubWebcam extends LinearOpMode {
 
         waitForStart();
 
+        while (opModeIsActive())
+        {
+            /*
+             * Send some stats to the telemetry
+             */
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
+
+            /*
+             * NOTE: stopping the stream from the camera early (before the end of the OpMode
+             * when it will be automatically stopped for you) *IS* supported. The "if" statement
+             * below will stop streaming from the camera when the "A" button on gamepad 1 is pressed.
+             */
+            if(gamepad1.a)
+            {
+                /*
+                 * IMPORTANT NOTE: calling stopStreaming() will indeed stop the stream of images
+                 * from the camera (and, by extension, stop calling your vision pipeline). HOWEVER,
+                 * if the reason you wish to stop the stream early is to switch use of the camera
+                 * over to, say, Vuforia or TFOD, you will also need to call closeCameraDevice()
+                 * (commented out below), because according to the Android Camera API documentation:
+                 *         "Your application should only have one Camera object active at a time for
+                 *          a particular hardware camera."
+                 *
+                 * NB: calling closeCameraDevice() will internally call stopStreaming() if applicable,
+                 * but it doesn't hurt to call it anyway, if for no other reason than clarity.
+                 *
+                 * NB2: if you are stopping the camera stream to simply save some processing power
+                 * (or battery power) for a short while when you do not need your vision pipeline,
+                 * it is recommended to NOT call closeCameraDevice() as you will then need to re-open
+                 * it the next time you wish to activate your vision pipeline, which can take a bit of
+                 * time. Of course, this comment is irrelevant in light of the use case described in
+                 * the above "important note".
+                 */
+                webcam.stopStreaming();
+                //webcam.closeCameraDevice();
+            }
+
+            /*
+             * For the purposes of this sample, throttle ourselves to 10Hz loop to avoid burning
+             * excess CPU cycles for no reason. (By default, telemetry is only sent to the DS at 4Hz
+             * anyway). Of course in a real OpMode you will likely not want to do this.
+             */
+            sleep(100);
+        }
+
 //        while (opModeIsActive()) {
 //            telemetry.addData("Analysis", pipeline.getAnalysis());
 //            telemetry.addData("Position", pipeline.position);

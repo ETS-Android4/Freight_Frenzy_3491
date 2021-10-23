e343ec8affd0b3fb874805d7bf276e4bc493b53d
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2021-10-23 10:55:05 -0700

    Base Autonomous Programs (needs to be calibrated, and fix NullPointerException.)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/Autonomous/PathingOne_P1_Blue.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Competition.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Robots.Ducky;
+import org.firstinspires.ftc.teamcode.opMode_Support.AutoOpMode;
+
+
+@Autonomous(name="PathingOne - P1, Blue")
+
+public class PathingOne_P1_Blue extends AutoOpMode {
+
+    // Initializing Robot Class
+    Ducky ducky;
+
+    @Override
+    public void runOp() throws InterruptedException {
+
+        // Wait for the game to begin
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
+
+        // Waiting for the program to start
+        waitForStart();
+
+        // Autonomous Pathing
+        ducky.DriveForward(1);
+        sleep(1000);
+        ducky.TurnLeft(1);
+        sleep(400);
+        ducky.DriveForward(1);
+        sleep(2000);
+        ducky.Stop();
+    }
+}

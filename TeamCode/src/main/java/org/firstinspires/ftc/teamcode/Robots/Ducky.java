c4ed542ca1f4a8018d153a951f0da532fac8b0ba
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2021-10-24 21:19:13 -0700

    Updated Ducky class.
    All nullPointerException theoretically fixed.
    
    Added optimizations and modifications to the class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Ducky.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Ducky.java
@@ -6,17 +6,24 @@ import static android.os.SystemClock.sleep;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Ducky {
 
     // Declaring Motor Variables
     public DcMotor FrontLeft, BackLeft, FrontRight, BackRight;
 
-    HardwareMap hwMap =  null;
-    private ElapsedTime period  = new ElapsedTime();
-    
-    // Declaring stuff
+    // Declaring opMode Variables
+    HardwareMap hwMap;
+
+    // Encoder + Wheel Declaration
+    public static final double MOTOR_TICK_COUNTS_PER_REV = 0; // Encoder counts per Motor (5203-2402-0019) Revolution
+
+    public static final double OMNI_WHEEL_DIAMETER =  96; // Diameter of Omni Wheel in mm
+    public static final double CART_WHEEL_DIAMETER = 127; // Diameter of Cart Wheel in mm
+
+
+    // Class Constructor
     public Ducky(){
 
     }
@@ -26,7 +33,7 @@ public class Ducky {
         // Save reference to Hardware map
         hwMap = ahwMap;
 
-        // Drive Motors
+        // Define Drive Motors
         FrontLeft = hwMap.dcMotor.get("frontL");
         BackLeft = hwMap.dcMotor.get("backL");
         FrontRight = hwMap.dcMotor.get("frontR");
@@ -38,45 +45,60 @@ public class Ducky {
         FrontRight.setDirection(DcMotor.Direction.FORWARD);
         BackRight.setDirection(DcMotor.Direction.FORWARD);
 
-        // Motor set Power
+        // Define Servos
+//        leftClaw  = hwMap.get(Servo.class, "left_hand");
+
+        // Initialize Servos
+//        leftClaw.setPosition(MID_SERVO);
+
+        // Motor set Power at init
         FrontLeft.setPower(0);
         BackLeft.setPower(0);
         FrontRight.setPower(0);
         BackRight.setPower(0);
 
-        FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Setting Motors to run with/ without Encoders - (RUN_WITHOUT_ENCODER/ RUN_USING_ENCODER)
+        FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     /**
      * Autonomous
      */
     // Robot Driving
-    public void DriveForward(double speed){
+    public void DriveForward(double speed, int milliseconds){
         FrontLeft.setPower(speed);
-        BackLeft.setPower(speed);
+        BackLeft.setPower(speed/ 0.7559);
         FrontRight.setPower(speed);
-        BackRight.setPower(speed);
+        BackRight.setPower(speed/ 0.7559);
+        sleep(milliseconds);
+        Stop();
     }
-    public void DriveBackward(double speed){
+    public void DriveBackward(double speed, int milliseconds){
         FrontLeft.setPower(-speed);
-        BackLeft.setPower(-speed);
+        BackLeft.setPower(-speed/ 0.7559);
         FrontRight.setPower(-speed);
-        BackRight.setPower(-speed);
+        BackRight.setPower(-speed/ 0.7559);
+        sleep(milliseconds);
+        Stop();
     }
-    public void TurnLeft(double speed){
+    public void TurnLeft(double speed, int milliseconds){
         FrontLeft.setPower(-speed);
-        BackLeft.setPower(-speed);
+        BackLeft.setPower(-speed/ 0.7559);
         FrontRight.setPower(speed);
-        BackRight.setPower(speed);
+        BackRight.setPower(speed/ 0.7559);
+        sleep(milliseconds);
+        Stop();
     }
-    public void TurnRight(double speed){
+    public void TurnRight(double speed, int milliseconds){
         FrontLeft.setPower(speed);
-        BackLeft.setPower(speed);
+        BackLeft.setPower(speed/ 0.7559);
         FrontRight.setPower(-speed);
-        BackRight.setPower(-speed);
+        BackRight.setPower(-speed/ 0.7559);
+        sleep(milliseconds);
+        Stop();
     }
     public void Stop(){
         FrontLeft.setPower(0);

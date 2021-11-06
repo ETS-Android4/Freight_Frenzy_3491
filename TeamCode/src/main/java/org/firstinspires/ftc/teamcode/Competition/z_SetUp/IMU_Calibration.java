68054388c20d089c2c00049b95ad585d77471705
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2021-11-06 12:46:32 -0700

    Created program to calibrate IMU.
    Program modified from provided IMU calibration program within the Qualcomm library.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/z_SetUp/IMU_Calibration.java
@@ -0,0 +1,130 @@
+/*
+ * Program taken from provided example programs within the Qualcomm library.
+ * Modified by team FIX IT 3491.
+ */
+
+package org.firstinspires.ftc.teamcode.Competition.z_SetUp;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.firstinspires.ftc.teamcode.Robots.Ducky;
+
+import java.io.File;
+import java.util.Locale;
+
+@TeleOp(name="IMU Calibration")
+
+public class IMU_Calibration extends LinearOpMode {
+
+
+    // Initializing Robot Class
+    Ducky ducky = new Ducky();
+
+    // State used for updating telemetry
+    Orientation angles;
+
+    @Override
+    public void runOpMode() {
+
+        /* Initializing the Program */
+        // Initialize all motors/ servos
+        ducky.init(hardwareMap);
+
+        // Telemetry logs
+        telemetry.log().setCapacity(12);
+        telemetry.log().add("");
+        telemetry.log().add("Please refer to the calibration instructions");
+        telemetry.log().add("contained in the Adafruit IMU calibration");
+        telemetry.log().add("sample OpMode.");
+        telemetry.log().add("");
+        telemetry.log().add("When sufficient calibration has been reached,");
+        telemetry.log().add("press the 'A' button to write the current");
+        telemetry.log().add("calibration data to a file.");
+        telemetry.log().add("");
+
+        // We are expecting the IMU to be attached to an I2C port on a Core Device Interface Module and named "imu".
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.loggingEnabled = true;
+        parameters.loggingTag     = "IMU";
+        ducky.imu.initialize(parameters);
+
+        composeTelemetry();
+        telemetry.log().add("Waiting for start...");
+
+        // Wait until we're told to go
+        while (!isStarted()) {
+            telemetry.update();
+            idle();
+        }
+
+        telemetry.log().add("...started...");
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.a) {
+
+                // Get the calibration data
+                BNO055IMU.CalibrationData calibrationData = ducky.imu.readCalibrationData();
+
+                // Save the calibration data to a file. You can choose whatever file
+                // name you wish here, but you'll want to indicate the same file name
+                // when you initialize the IMU in an OpMode in which it is used. If you
+                // have more than one IMU on your robot, you'll of course want to use
+                // different configuration file names for each.
+                String filename = "AdafruitIMUCalibration.json";
+                File file = AppUtil.getInstance().getSettingsFile(filename);
+                ReadWriteFile.writeFile(file, calibrationData.serialize());
+                telemetry.log().add("saved to '%s'", filename);
+
+                // Wait for the button to be released
+                while (gamepad1.a) {
+                    telemetry.update();
+                    idle();
+                }
+            }
+
+            telemetry.update();
+        }
+    }
+
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(() -> {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = ducky.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        });
+
+        telemetry.addLine()
+                .addData("status", () -> ducky.imu.getSystemStatus().toShortString())
+                .addData("calibration", () -> ducky.imu.getCalibrationStatus().toString());
+
+        telemetry.addLine()
+                .addData("heading", () -> formatAngle(angles.angleUnit, angles.firstAngle))
+                .addData("roll", () -> formatAngle(angles.angleUnit, angles.secondAngle))
+                .addData("pitch", () -> formatAngle(angles.angleUnit, angles.thirdAngle));
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // Formatting
+    //----------------------------------------------------------------------------------------------
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}

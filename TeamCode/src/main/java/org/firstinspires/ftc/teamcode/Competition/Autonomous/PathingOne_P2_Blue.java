c68c05ec03fdd146beb948cddb2519f8d76d9d2e
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2021-11-10 11:26:06 -0800

    Update PathingOne_P2_Blue.java
    Program used for testing Encoders, both with calling function using instance of Ducky class, or directly within program. (Testing using Telemetry from Ducky class.)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/Autonomous/PathingOne_P2_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/Autonomous/PathingOne_P2_Blue.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.Robots.Ducky;
+import org.firstinspires.ftc.teamcode.Season_Setup.Ducky;
 
 
 @Autonomous(name="PathingOne - P2, Blue")
@@ -18,7 +18,7 @@ public class PathingOne_P2_Blue extends LinearOpMode {
     public void runOpMode() {
 
         // Initialize all motors/ servos
-        ducky.init(hardwareMap);
+        ducky.init(hardwareMap, telemetry);
 
         // Reset Encoders, and Telemetry Update
         telemetry.addData("Status", "Resetting Encoders");
@@ -40,7 +40,38 @@ public class PathingOne_P2_Blue extends LinearOpMode {
         waitForStart();
 
         // Autonomous Pathing
-        ducky.DriveForward_Encoder(1,0.5);
+        int Cart_Wheel_Distance = 68;
+
+        ducky.BackLeft.setTargetPosition(Cart_Wheel_Distance);
+        ducky.BackRight.setTargetPosition(Cart_Wheel_Distance);
+
+        ducky.BackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        ducky.BackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        ducky.FrontLeft.setPower(-1);
+        ducky.BackLeft.setPower(-1/ 0.7559);
+        ducky.FrontRight.setPower(-1);
+        ducky.BackRight.setPower(-1/ 0.7559);
+
+        while (ducky.BackLeft.isBusy() || ducky.BackRight.isBusy()) {
+            telemetry.addData("Driving Forward, Encoder Pulses Left (Left Wheel)",
+                    Cart_Wheel_Distance-ducky.BackLeft.getCurrentPosition());
+            telemetry.addData("Driving Forward, Encoder Pulses Left (Right Wheel)",
+                    Cart_Wheel_Distance-ducky.BackRight.getCurrentPosition());
+            telemetry.update();
+        }
+
+        ducky.FrontLeft.setPower(0);
+        ducky.BackLeft.setPower(0);
+        ducky.FrontRight.setPower(0);
+        ducky.BackRight.setPower(0);
+
+        telemetry.addData("Encoder Position",  "Starting Encoder Position",
+                ducky.BackLeft.getCurrentPosition(),
+                ducky.BackRight.getCurrentPosition());
+        telemetry.update();
+
+//        ducky.DriveForward_Encoder(4,0.5);
 
     }
 }

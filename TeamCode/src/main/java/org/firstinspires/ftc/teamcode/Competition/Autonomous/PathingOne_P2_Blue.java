32f3c733553facc1148785fe2f351d0ed7bb91db
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2021-11-20 00:40:24 -0800

    Adding Pathing to Program (untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/Autonomous/PathingOne_P2_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/Autonomous/PathingOne_P2_Blue.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode.Competition.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Season_Setup.Ducky;
+import org.firstinspires.ftc.teamcode.Season_Setup.Freight_Frenzy_Pipeline;
 
 
 @Autonomous(name="PathingOne - P2, Blue", group="Competition - Blue")
@@ -19,6 +21,7 @@ public class PathingOne_P2_Blue extends LinearOpMode {
 
         // Initialize all motors/ servos
         ducky.init(hardwareMap, telemetry);
+        Freight_Frenzy_Pipeline freight_frenzy_pipeline = new Freight_Frenzy_Pipeline();
 
         // Reset Encoders, and Telemetry Update
         telemetry.addData("Status", "Resetting Encoders");
@@ -38,18 +41,30 @@ public class PathingOne_P2_Blue extends LinearOpMode {
         waitForStart();
 
         // Autonomous Pathing
-        ducky.DriveBackward_Encoder_IMU(10,-0.3);
+        ducky.DriveBackward_Encoder(1,-0.3);
         Thread.sleep(1000);
-        ducky.TurnRight_IMU(45,0.3);
+        ducky.turn_P(-45,4000);
+
+        if (Freight_Frenzy_Pipeline.positionOfTeamShippingElement == Freight_Frenzy_Pipeline.Pipeline.ElementPosition.LEFT) {
+            ducky.ArmBottomLevel();
+        } else if (Freight_Frenzy_Pipeline.positionOfTeamShippingElement == Freight_Frenzy_Pipeline.Pipeline.ElementPosition.CENTER) {
+            ducky.ArmMidLevel();
+        } else if (Freight_Frenzy_Pipeline.positionOfTeamShippingElement == Freight_Frenzy_Pipeline.Pipeline.ElementPosition.RIGHT) {
+            ducky.ArmTopLevel();
+        }
+        ducky.DriveBackward_Encoder(2,0.5);
+        ducky.CollectorReverse();
         Thread.sleep(1000);
-        ducky.DriveBackward_Encoder_IMU(10,-0.3);
-        Thread.sleep(1000);
-        ducky.TurnRight_IMU(90,0.3);
-        Thread.sleep(1000);
-        ducky.DriveForward_Encoder_IMU(13,0.3);
-        Thread.sleep(1000);
+        ducky.CollectorOff();
+        ducky.ArmCollecting();
+        ducky.turn_P(-45,4000);
+        ducky.ArmRotator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ducky.DriveForward_Encoder(20,0.5);
+        ducky.turn_P(-90,4000);
+        ducky.DriveBackward_Encoder(10,0.5);
         ducky.CarouselSpinnerOn();
-        Thread.sleep(3000);
-        ducky.CarouselSpinnerOff();
+        Thread.sleep(4000);
+        ducky.turn_P(-90,4000);
+        ducky.DriveForward_Encoder(144,1);
     }
 }

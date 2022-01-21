cf8a702ecc274b854b723c6ee0cb63b4cdf5ce07
FIXIT-3491 <97809407+FIXIT-3491@users.noreply.github.com>
2022-01-20 19:46:50 -0800

    Create DriveToWarehouse.java
    Basic Program: Drives from point A to warehouse.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/Autonomous/DriveToWarehouse.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.Competition.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Season_Setup.Ducky;
+
+
+@Autonomous(name = "DriveToWarehouse - P1, Red", group="Competition - Red")
+
+public class DriveToWarehouse extends LinearOpMode{
+
+    // Initializing Robot Class
+    Ducky ducky = new Ducky();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Initialize all motors/ servos
+        ducky.init(hardwareMap, telemetry);
+
+        // Reset Encoders, and Telemetry Update
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        // Setting Alliance Colour for TeleOp
+        ducky.writeAndRead("Red");
+
+        // Encoder Position Update
+        telemetry.addData("Encoder Position",  "Starting Encoder Position",
+                ducky.backLeft.getCurrentPosition(),
+                ducky.backRight.getCurrentPosition());
+        telemetry.update();
+
+        // Setting Alliance Colour for TeleOp
+        ducky.writeAndRead("Red");
+
+        // Telemetry Update
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.addData("Alliance", Ducky.alliance);
+        telemetry.update();
+
+        // Waiting for the program to start
+        waitForStart();
+        // Autonomous Pathing
+        ducky.driveForward_Encoder(30,0.5,5000);
+        ducky.stop_Power();
+    }
+}
